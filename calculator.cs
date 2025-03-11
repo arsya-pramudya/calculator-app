cb946224e28e90133aa1c53f34b9c9c08d3909dd
arsya-pramudya <arsya.banyuwangi@students.amikom.ac.id>
2025-03-11 14:08:21 +0700

    Menambahkan fungsi Main

--- calculator.cs
+++ calculator.cs
@@ -1,6 +1,14 @@
 class calculator{
 	static void Main(String[] args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		    Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan (a,b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan (a,b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
 		
 	}
 	
